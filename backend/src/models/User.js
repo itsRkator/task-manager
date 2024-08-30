0e47e92b916713fe6e132da7da35ab587557338e
Rohitash Kator <rohitashkator@Rohitashs-MacBook-Pro.local>
2024-08-31 00:56:25 +0530

    Final CleanUp

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -3,15 +3,18 @@ const mongoose = require("mongoose");
 const defaultAvatarURL =
   "https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg?t=st=1725041453~exp=1725045053~hmac=be5fcdd487ee7dbbe38b6469e924f7d0de37e6fe7418a100e8521ecbcd1c4186&w=826";
 
-const userSchema = new mongoose.Schema({
-  firstName: { type: String, required: true },
-  lastName: { type: String, required: true },
-  email: { type: String, unique: true, required: true },
-  password: { type: String },
-  googleId: { type: String },
-  googleToken: { type: String },
-  googleTokenSecret: { type: String },
-  avatar: { type: String, default: defaultAvatarURL },
-}, {timestamps: true});
+const userSchema = new mongoose.Schema(
+  {
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, unique: true, required: true },
+    password: { type: String },
+    googleId: { type: String },
+    googleToken: { type: String },
+    googleTokenSecret: { type: String },
+    avatar: { type: String, default: defaultAvatarURL },
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("User", userSchema);
